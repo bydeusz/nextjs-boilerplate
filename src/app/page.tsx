3db3122d86372d94702bb18212476e11f09efc98
Tadeusz de Ruijter <tech@nomadventrs.com>
2024-12-15 01:37:29 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="p-9">
+      <h1 className="text-xl font-bold">Hello World</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+import forms from '@tailwindcss/forms';
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E3EE6",
+        secondary: "#1832B8",
+      },
+    },
+  },
+  plugins: [forms],
+};
+export default config;
