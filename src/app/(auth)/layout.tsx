4b91df32eedddf2c432c8b59c1b1620adf0b96e2
Tadeusz de Ruijter <tech@nomadventrs.com>
2024-12-17 16:48:19 +0100

    feat: added layout for auth pages

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex w-full h-screen">
+      <div className="hidden lg:flex lg:w-1/3 xl:w-1/2">
+        <Image
+          src="/img/mood-bg.jpg"
+          alt="This is showing a mood background for the login screen"
+          width={500}
+          height={500}
+          layout="responsive"
+        />
+      </div>
+      <div className="flex w-full lg:w-2/3 xl:w-1/2 items-center bg-gray-100 justify-center p-4 md:p-0">
+        <div className="lg:w-1/2 bg-white rounded-md p-8 shadow-2xl">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -33,36 +33,34 @@ export default function LoginForm() {
   }
 
   return (
-    <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-      <div className="rounded-md shadow-sm -space-y-px">
-        <div>
-          <label htmlFor="email" className="sr-only">
-            Email address
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-            placeholder="Email address"
-          />
-        </div>
-        <div>
-          <label htmlFor="password" className="sr-only">
-            Password
-          </label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            required
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-            placeholder="Password"
-          />
-        </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email" className="sr-only">
+          Email address
+        </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          autoComplete="email"
+          required
+          className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+          placeholder="Email address"
+        />
+      </div>
+      <div>
+        <label htmlFor="password" className="sr-only">
+          Password
+        </label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          autoComplete="current-password"
+          required
+          className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+          placeholder="Password"
+        />
       </div>
 
       {error && <div className="text-red-500 text-sm text-center">{error}</div>}
