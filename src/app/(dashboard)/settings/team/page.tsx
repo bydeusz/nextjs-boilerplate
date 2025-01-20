6bacca316917dc7015e5b358793b4601c28bdb4e
Tadeusz de Ruijter <tech@nomadventrs.com>
2025-01-20 20:05:44 +0100

    chore: moved teamlist

--- src/app/(dashboard)/settings/team/page.tsx
+++ src/app/(dashboard)/settings/team/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { auth } from "@/config/auth";
 import { prisma } from "@/config/prisma";
 
-import { TeamList } from "@/components/Lists/TeamList/TeamList";
+import { TeamList } from "@/components/team/TeamList/TeamList";
 
 export const metadata: Metadata = {
   title: "Team - Next JS Dashboard Boilerplate by @bydeusz.com",
